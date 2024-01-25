c2f0de1e8d42ce9a4e04188fed66c7ed598b0210
vittorioaugusto <vi.aborges47@gmail.com>
2024-01-25 12:51:40 -0300

    update 2

---
+++ script.js
@@ -0,0 +1,34 @@
+const greetingElement = document.getElementById("greeting");
+
+const currentHour = new Date().getHours();
+
+if (currentHour >= 5 && currentHour < 12) {
+  greetingElement.textContent = "Bom dia";
+} else if (currentHour >= 12 && currentHour < 18) {
+  greetingElement.textContent = "Boa tarde";
+} else {
+  greetingElement.textContent = "Boa noite";
+}
+
+const greetingMessage =
+  currentHour >= 5 && currentHour < 12
+    ? "Bom dia"
+    : currentHour >= 12 && currentHour < 18
+    ? "Boa tarde"
+    : "Boa noite";
+
+greetingElement.textContent = greetingMessage;
+
+const container = document.querySelector(".offer__list-item");
+
+const observer = new ResizeObserver(() => {  
+  const containerWidth = container.offsetWidth;
+  const numColumns = Math.floor(containerWidth / 200);
+
+  container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(200px, 1fr))`;
+
+  console.log({ container });
+  console.log({ numColumns });
+});
+
+observer.observe(container);
